eee123e18c7310ed3840b5e125aa6d6a1bb29fd7
bob <robert.matthew.brooks@gmail.com>
2023-08-16 11:56:35 +0100

    format json output

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const endpoints = require('./endpoints.json');
 
 const app = express();
+app.set('json spaces', 2);
 
 app.get('/api', (req, res, next) => {
     res.status(200).send(endpoints);
