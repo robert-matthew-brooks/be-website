9c895549d83615881c2cb2946c24cab16f198980
bob <robert.matthew.brooks@gmail.com>
2023-09-18 21:54:13 +0100

    add cron job to keep server awake

---
+++ keep-awake.js
@@ -0,0 +1,18 @@
+const CronJob = require('cron').CronJob;
+const https = require('https');
+
+const serverUrl = 'https://be-website.onrender.com/';
+
+const keepAwake = new CronJob('*/10 * * * * *', () => {
+  console.log('Pinging server...');
+
+  https
+    .get(serverUrl, (res) => {
+      console.log(`Server responded with code: ${res.statusCode}`);
+    })
+    .on('error', (err) => {
+      console.log(`Server responded with error: ${err.message}`);
+    });
+});
+
+module.exports = keepAwake;
