01e37136eacc2ffd3de5568b861fe4fe044a1cc0
bob <robert.matthew.brooks@gmail.com>
2023-09-18 11:00:57 +0100

    prepare for prod server

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,17 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 const ENV = process.env.NODE_ENV || 'development';
+const config = {};
 
 dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('Database not found');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
