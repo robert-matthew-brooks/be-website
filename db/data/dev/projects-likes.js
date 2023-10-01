e3db8c7e73c1bbc65f3aa117691788a937057182
bob <robert.matthew.brooks@gmail.com>
2023-10-01 17:21:03 +0100

    refactor variable names

--- db/data/dev/projects-likes.js
+++ db/data/dev/projects-likes.js
@@ -5,7 +5,7 @@ const projectLikesData = [];
 for (const project of projectData) {
   const project_id = projectData.indexOf(project) + 1;
 
-  for (const ip_address of project.liked_ips) {
+  for (const ip_address of project.likes_ips) {
     projectLikesData.push({
       project_id,
       ip_address,
